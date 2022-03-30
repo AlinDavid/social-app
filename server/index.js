ce637bed070f23daeaafdf864c09278df0e1a7f1
Alin <alin.constantin1695@gmail.com>
2022-03-30 09:44:19 +0300

    mongodb connection

--- server/index.js
+++ server/index.js
@@ -4,3 +4,21 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  'mongodb+srv://adavid:Aa1212!A1@cluster0.mes34.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`)),
+  )
+  .catch((err) => console.error(err));
